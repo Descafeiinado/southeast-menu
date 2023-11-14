0e7eb8208fbdea44f9e0f78bd8d04a680ed5385c
Heaven <38844010+Descafeiinado@users.noreply.github.com>
2023-11-14 09:48:28 -0300

    chore: update mispelled project name and remove product

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ export function Footer() {
   return <footer className="flex mt-8 h-20 w-full items-center p-7 bg-[#0D1D25] justify-between">
     <img src={logoImg} alt="Maanaim" />
 
-
     <p>
       Feito com <span className="text-[#EE0000]">♥</span> por <b><a href="https://github.com/Descafeiinado" target="_blank" rel="noreferrer">Júlio</a></b> e <b><a href="https://github.com/okiwiziin" target="_blank" rel="noreferrer">Otávio</a></b>
     </p>
